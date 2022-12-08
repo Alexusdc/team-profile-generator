4b051d3a604d753d4da5c01aa63d0a73c60c553c
alexuschristensen <alexusdc6@gmail.com>
2022-12-07 21:52:05 -0800

    add intern

--- index.js
+++ index.js
@@ -37,66 +37,100 @@ function initializeTeam() {
             // idArray.push(answers.managerId);
            return createTeam();
         });
-};
-
-
-function createTeam() {
-    inquirer.promt([
-        {
-        type: 'list',
-        name: 'managerChoice',
-        message: 'Which of the following would you like to add?',
-        choices: [
-            'Engineer',
-            'Intern',
-            'Finish building team'
-        ]}
-    ]).then (userChoice => {
-        switch (userChoice.memberChoice){
-            case "Engineer":
-                addEngineer();
-                break;
-            case "Intern":
-                addIntern();
-                break;
-            default:
-                buildTeam();
-        };
-    });
-};
-
-function addEngineer(){
-    inquirer.prompt([ {
-        type: "input",
-        name: "engineerName",
-        message: "What is the engineer's name?",
-    },
-
-    {
-        type: "input",
-        name: "engineerId",
-        message: "What is the engineer's ID?",
-    },
-
-    {
-        type: "input",
-        name: "engineerEmail",
-        message: "What is the engineer's email?",
-    },
-
-    {
-        type: "input",
-        name: "engineerGithub",
-        message: "What is the engineer's github?",
     }
 
-]).then(answers => {
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-        engineer.special = answers.engineerGithub
-        teamMembers.push(engineer);
-       return createTeam();
-    });
-}
+    function createTeam() {
+        inquirer.promt([
+            {
+            type: 'list',
+            name: 'managerChoice',
+            message: 'Which of the following would you like to add?',
+            choices: [
+                'Engineer',
+                'Intern',
+                'Finish building team'
+            ]}
+        ]).then (userChoice => {
+            switch (userChoice.memberChoice){
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    buildTeam();
+            };
+        });
+    };
 
+    function addEngineer(){
+        inquirer.prompt([ {
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name?",
+        },
+
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's ID?",
+        },
+
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?",
+        },
+
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's github?",
+        }
+
+    ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            engineer.special = answers.engineerGithub
+            teamMembers.push(engineer);
+        return createTeam();
+        });
+    }
+
+    function addIntern(){
+        inquirer.prompt([ {
+            type: "input",
+            name: "internName",
+            message: "What is the Intern's name?",
+        },
+
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the interns's ID?",
+        },
+
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email?",
+        },
+
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school?",
+        }
+
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        intern.special = answers.internSchool
+        teamMembers.push(intern);
+        // idArray.push(answers.internId);
+        return createTeam();
+    })
+
+    createManager();
+}
 
 module.exports = initializeTeam; 
