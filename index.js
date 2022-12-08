9c26fc557e12cf07352883cf2dea372df2abb564
alexuschristensen <alexusdc6@gmail.com>
2022-12-08 00:14:22 -0800

    html bugs corrected

--- index.js
+++ index.js
@@ -26,12 +26,13 @@ function buildTeam(){
 <head> <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>My Team</title>
 </head>
 <body>
-    <di>
-        <div>
-            <di>
+    <div class="">
+        <div class="">
+            <div class="">
     `);
     for (let i = 0; i < teamMembers.length; i++) {
         console.log(teamMembers[i])
@@ -44,11 +45,11 @@ function buildTeam(){
              <div>
                 <ul class="list-group">
                     <li> ID: ${teamMembers[i].id}</li>
-                    <li?> Email: <a href="mailto: ${teamMembers[i].email}">${teamMembers[i].email}</a></li>
+                    <li> Email: <a href="mailto: ${teamMembers[i].email}">${teamMembers[i].email}</a></li>
                     <li>  ${teamMembers[i].special}</li>
                 </ul>
-            <div>
-        <div> 
+            </div>
+        </div> 
         `
         
         )
