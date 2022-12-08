b481fe269b71d61df82343cf55d853f00e2a95a0
alexuschristensen <alexusdc6@gmail.com>
2022-12-07 21:29:20 -0800

    debugged and added create team function

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
 
-function Team() {
-    initializeTeam() {
+function initializeTeam() {
         inquirer
-        .prompt({
+        .prompt([
+            {
             type: 'input', 
             name: 'name',
             message: 'Please enter managers name',
@@ -29,17 +29,42 @@ function Team() {
             type: 'input',
             name: 'officeNumber',
             message: 'Please enter office number',
-        })
-        .then(answers => {
+        },
+        ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             manager.special = answers.managerOfficeNumber
             teamMembers.push(manager);
             // idArray.push(answers.managerId);
            return createTeam();
         });
-    }
+};
 
 
-}
+function createTeam() {
+    inquirer.promt([
+        {
+        type: 'list',
+        name: 'managerChoice',
+        message: 'Which of the following would you like to add?',
+        choices: [
+            'Engineer',
+            'Intern',
+            'Finish building team'
+        ]}
+    ]).then (userChoice => {
+        switch (userChoice.memberChoice){
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                buildTeam();
+        };
+    });
+};
 
-module.exports = Team; 
+
+
+module.exports = initializeTeam; 
